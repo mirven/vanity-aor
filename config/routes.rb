4734e4c4c07d2e82fa38371bc51fc7200affab61
Marcus Irven <marcus@marcusirven.com>
2010-09-28 15:43:10 -0500

    add vanity dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => "home"
   map.cool "cool", :controller => "home", :action => "cool"
   map.about "about", :controller => "home", :action => "about"
+  map.vanity "/vanity/:action/:id", :controller => :vanity
 end
